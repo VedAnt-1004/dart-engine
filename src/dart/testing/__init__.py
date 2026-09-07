5065a1269115cd712635de9f5b9725fba7aeb112
VedAnt-1004 <vedantmandavkar1004@gmail.com>
2026-09-07 14:53:35 +0530

    Add test-support tooling for configurable mock chaos receiver in `dart` package

---
+++ src/dart/testing/__init__.py
@@ -0,0 +1,3 @@
+"""Test-support tooling shipped as part of the `dart` package: currently
+the configurable mock chaos receiver used to exercise dispatch
+resilience end-to-end without a real third-party endpoint."""
